dcbfd38c8cf64c8e4b119c26b1b8f44bee813845
Sayaka Vincent <sfv2010@hotmail.fr>
2023-04-25 00:28:46 +0200

    Add !Array.from

--- scripts/utils/recipeUtil.js
+++ scripts/utils/recipeUtil.js
@@ -1,25 +1,55 @@
 export function getIngredientList(recipes) {
+    const targetTags = document.querySelectorAll(".tag.target.ingredients");
     let arrayIngredients = [];
     recipes.forEach((recipe) => {
         recipe.ingredients.forEach((ingredientKey) => {
-            arrayIngredients.push(ingredientKey.ingredient.toLowerCase());
+            if (targetTags) {
+                if (
+                    !Array.from(targetTags).find((tag) => {
+                        return tag.textContent
+                            .toLowerCase()
+                            .includes(ingredientKey.ingredient.toLowerCase());
+                    })
+                )
+                    arrayIngredients.push(
+                        ingredientKey.ingredient.toLowerCase()
+                    );
+            } else {
+                arrayIngredients.push(ingredientKey.ingredient.toLowerCase());
+            }
         });
     });
     return arrayIngredients;
 }
 export function getApplianceList(recipes) {
+    const targetTags = document.querySelectorAll(".tag.target.appliances");
     let arrayAppliances = [];
     recipes.forEach((recipe) => {
-        arrayAppliances.push(recipe.appliance);
+        if (
+            !Array.from(targetTags).find((tag) => {
+                return tag.textContent
+                    .toLowerCase()
+                    .includes(recipe.appliance.toLowerCase());
+            })
+        )
+            arrayAppliances.push(recipe.appliance);
     });
     return arrayAppliances;
 }
 
 export function getUstensileList(recipes) {
+    const targetTags = document.querySelectorAll(".tag.target.ustensiles");
     let arrayUstensils = [];
     recipes.forEach((recipe) => {
         recipe.ustensils.forEach((ustensilKey) => {
-            arrayUstensils.push(ustensilKey.replace(/[^a-z]/gi, ""));
+            if (
+                !Array.from(targetTags).find((tag) => {
+                    return tag.textContent
+                        .toLowerCase()
+                        .includes(ustensilKey.toLowerCase());
+                })
+            )
+                arrayUstensils.push(ustensilKey.replace(/[(]\d[)]/gi, ""));
         });
     });
     return arrayUstensils;
